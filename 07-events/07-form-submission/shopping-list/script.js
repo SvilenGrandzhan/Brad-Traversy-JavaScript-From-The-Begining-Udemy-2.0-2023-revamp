61deec64b2ab9edd42ac4c45b14b6121c2bc6cae
Svilen Grandzhan <svileng.grandzhan@gmail.com>
2023-05-10 16:27:07 -0700

    Work commit 20230510
    FormData and small adds to inputs

---
+++ 07-events/07-form-submission/shopping-list/script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('item-form')
+const onSubmit = (e) => {
+  e.preventDefault()
+  const item = document.getElementById('item-input').value
+  const priority = document.getElementById('priority-input')
+  console.log(item, priority.value)
+}
+
+const onSubmit2 = (e) => {
+  e.preventDefault()
+  const formData = new FormData(form)
+  const item = formData.get('item')
+  // FormData is using name attribute of the form
+  const priority = formData.get('priority')
+  // FormData is using name attribute of the form
+  const entries = formData.entries()
+  //   console.log(item, priority)
+  //   console.log(entries)
+  //   // We get an Iterator
+  //   // We can use for of loop
+  for (const entry of entries) {
+    console.log(entry)
+  }
+}
+
+// form.addEventListener('submit', onSubmit)
+form.addEventListener('submit', onSubmit2)
